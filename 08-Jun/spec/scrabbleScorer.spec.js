523b352ff3a4a7d1595dc7372553791e705d81e2
Flora Davies <daviesfgo@gmail.com>
2021-06-09 17:40:55 +0100

    Passed second test: different letter with different score, still hardcoded

--- 08-Jun/spec/scrabbleScorer.spec.js
+++ 08-Jun/spec/scrabbleScorer.spec.js
@@ -4,4 +4,12 @@ describe("scrabbleScorer", () => {
   test("it scores 1 for letter a", () => {
     expect(scrabbleScorer("a")).toEqual(1);
   });
+
+  test("it scores 2 for letter d", () => {
+    expect(scrabbleScorer("d")).toEqual(2);
+  });
+
+  test("it scores 1 for letter e", () => {
+    expect(scrabbleScorer("e")).toEqual(1);
+  });
 });
--- 08-Jun/src/scrabbleScorer.js
+++ 08-Jun/src/scrabbleScorer.js
@@ -1,5 +1,9 @@
 function scrabbleScorer(word) {
-  return 1;
+  if (word === "a") {
+    return 1;
+  } else if (word === "d") {
+    return 2;
+  }
 }
 
 module.exports = scrabbleScorer;
