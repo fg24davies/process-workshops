d11dbdaba0854e32d36b20d46ea3d1bbc68714e5
Flora Davies <daviesfgo@gmail.com>
2021-06-10 11:04:15 +0100

    Passing tests for letters of scores 3

--- 08-Jun/spec/scrabbleScorer.spec.js
+++ 08-Jun/spec/scrabbleScorer.spec.js
@@ -11,4 +11,37 @@ describe("scrabbleScorer", () => {
     expect(scrabbleScorer("d")).toEqual(2);
     expect(scrabbleScorer("g")).toEqual(2);
   });
+
+  test("it scores 3 for letters bcmp", () => {
+    expect(scrabbleScorer("b")).toEqual(3);
+    expect(scrabbleScorer("c")).toEqual(3);
+    expect(scrabbleScorer("m")).toEqual(3);
+    expect(scrabbleScorer("p")).toEqual(3);
+  });
+
+  test("it scores 4 for letters fhvwy ", () => {
+    expect(scrabbleScorer("f")).toEqual(4);
+    expect(scrabbleScorer("h")).toEqual(4);
+    expect(scrabbleScorer("v")).toEqual(4);
+    expect(scrabbleScorer("w")).toEqual(4);
+    expect(scrabbleScorer("y")).toEqual(4);
+  });
+
+  // test("it scores 5 for letters aeioulnrst ", () => {
+  //   expect(scrabbleScorer("a")).toEqual(1);
+  //   expect(scrabbleScorer("i")).toEqual(1);
+  //   expect(scrabbleScorer("e")).toEqual(1);
+  // });
+
+  // test("it scores 8 for letters aeioulnrst ", () => {
+  //   expect(scrabbleScorer("a")).toEqual(1);
+  //   expect(scrabbleScorer("i")).toEqual(1);
+  //   expect(scrabbleScorer("e")).toEqual(1);
+  // });
+
+  // test("it scores 10 for letters aeioulnrst ", () => {
+  //   expect(scrabbleScorer("a")).toEqual(1);
+  //   expect(scrabbleScorer("i")).toEqual(1);
+  //   expect(scrabbleScorer("e")).toEqual(1);
+  // });
 });
--- 08-Jun/src/scrabbleScorer.js
+++ 08-Jun/src/scrabbleScorer.js
@@ -3,6 +3,8 @@ function scrabbleScorer(word) {
     return 1;
   } else if (["d", "g"].includes(word)) {
     return 2;
+  } else if (["b", "c", "m", "p"].includes(word)) {
+    return 3;
   }
 }
 
