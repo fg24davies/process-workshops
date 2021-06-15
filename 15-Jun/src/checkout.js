795d7540bd5caf513148f02a4715c14f24aaa85e
Flora Davies <daviesfgo@gmail.com>
2021-06-15 11:09:14 +0100

    Refactored if/else statement to object

--- 15-Jun/src/checkout.js
+++ 15-Jun/src/checkout.js
@@ -1,14 +1,28 @@
 class Shop {
   checkout(shopping) {
-    if (shopping === "A") {
-      return 50;
-    } else if (shopping === "B") {
-      return 30;
-    } else if (shopping === "C") {
-      return 20;
-    } else if (shopping === "D") {
-      return 10;
-    }
+    const stock = {
+      A: 50,
+      B: 30,
+      C: 20,
+      D: 10,
+    };
+
+    let shoppingArray = shopping.split("");
+    return stock[shoppingArray[0]];
+
+    // for(let i = 0; i < shoppingArray.length; i++) {
+
+    // }
+
+    // if (shopping === "A") {
+    //   return 50;
+    // } else if (shopping === "B") {
+    //   return 30;
+    // } else if (shopping === "C") {
+    //   return 20;
+    // } else if (shopping === "D") {
+    //   return 10;
+    // }
   }
 }
 
