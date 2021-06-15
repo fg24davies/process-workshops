490a448c68a3b344a2699562c8600f77f43d62d4
Flora Davies <daviesfgo@gmail.com>
2021-06-15 11:21:52 +0100

    Refactored to count any number of items in shopping basket

--- 15-Jun/src/checkout.js
+++ 15-Jun/src/checkout.js
@@ -8,12 +8,17 @@ class Shop {
     };
 
     let shoppingArray = shopping.split("");
-
-    if (shoppingArray.length === 1) {
-      return stock[shoppingArray[0]];
-    } else {
-      return stock[shoppingArray[0]] + stock[shoppingArray[1]];
+    let total = 0;
+    for (let i = 0; i < shoppingArray.length; i++) {
+      total += stock[shoppingArray[i]];
     }
+    return total;
+
+    // if (shoppingArray.length === 1) {
+    //   return stock[shoppingArray[0]];
+    // } else {
+    //   return stock[shoppingArray[0]] + stock[shoppingArray[1]];
+    // }
   }
 }
 
